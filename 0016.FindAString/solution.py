6f196208328ead3d715404a9d1af288991890c7d
saumyagupta02 <52045656+saumyagupta02@users.noreply.github.com>
2020-08-28 14:23:56 +0530

    Update solution.py
    No need to write print. As the print is already mentioned in the main function.

--- 0016.FindAString/solution.py
+++ 0016.FindAString/solution.py
@@ -4,7 +4,7 @@ def count_substring(string, sub_string):
     count = 0
     for i in range(1,len(string)+1):
         for j in range(i):
-            print(j,i,string[j:i],sub_string)
+          
             if string[j:i] != "" and string[j:i] == sub_string: count+=1
     return count
 
